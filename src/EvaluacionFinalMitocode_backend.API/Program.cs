cf333e760a1341aaaed1b710027018c00f2c052d
Andres Evans <andres_evans@outlook.com>
2025-08-22 10:19:58 -0500

    Logger configured in Program.cs running and creating new log files

--- src/EvaluacionFinalMitocode_backend.API/Program.cs
+++ src/EvaluacionFinalMitocode_backend.API/Program.cs
@@ -1,17 +1,58 @@
+using Serilog;
+using Serilog.Events;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configuracion de Serilog
+var logPath = Path.Combine(AppContext.BaseDirectory, "logs", "log.txt");    // Ruta del archivo de log
+var logger = new LoggerConfiguration()
+    .WriteTo.Console()  // log to console
+    .WriteTo.File(logPath,
+        rollingInterval: RollingInterval.Day, // log file will roll daily
+        restrictedToMinimumLevel: LogEventLevel.Information, // Log level
+        retainedFileCountLimit: 7, // Keep logs for 7 days
+        fileSizeLimitBytes: 10 * 1024 * 1024, // Limit each log file to 10 MB
+        rollOnFileSizeLimit: true // Roll the file when it reaches the size limit
+        ).CreateLogger();
 
-builder.Services.AddControllers();
+try
+{
+    // Configurar Serilog como el logger de la aplicacion
+    // Cuando cargue la aplicacion, mostrara en que ambiente se esta ejecutando (Development, Staging, Production, etc.)
+    builder.Logging.AddSerilog(logger);
+    logger.Information($"LOG INITIALIZED in {Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "NO ENV"}");
 
-var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+    builder.Services.AddControllers();
+    // Agregar los servicios
+    //builder.Services.AddControllers(options =>          // Se agregan los controladores con las configuraciones de los filtros encontrados en FiltersExceptions.cs
+    //{
+    //    options.Filters.Add(typeof(FilterExceptions));
+    //});
 
-app.UseHttpsRedirection();
+    /**
+     * Para arriba estan los servicios!
+     * Desde var app hacia abajo se le conoce como el 'pipeline' de la aplicacion.
+     * Indicatr que elementos y funcionalidades se van a utilizar en la aplicacion.
+     */
 
-app.UseAuthorization();
+    var app = builder.Build();
 
-app.MapControllers();
 
-app.Run();
+
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+
+    app.Run();
+}
+catch (Exception ex) 
+{
+    logger.Fatal(ex, "Una excepcion fatal ocurrio al inicializar la API.");
+    throw;
+}
+finally
+{
+    Log.CloseAndFlush(); // Asegurarse de que todos los logs se escriban antes de cerrar la aplicacion. 
+}
