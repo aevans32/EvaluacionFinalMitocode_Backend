0f1103bf0dc5962b9055abdb274543305d0be434
Andres Evans <andres_evans@outlook.com>
2025-08-22 11:03:22 -0500

    Added swagger, modified Program.cs and launchSettings.json, also added contact info to the swagger page

--- src/EvaluacionFinalMitocode_backend.API/Program.cs
+++ src/EvaluacionFinalMitocode_backend.API/Program.cs
@@ -1,3 +1,5 @@
+using EvaluacionFinalMitocode_backend.API.Filters;
+using Microsoft.OpenApi.Models;
 using Serilog;
 using Serilog.Events;
 
@@ -24,12 +26,29 @@ try
     logger.Information($"LOG INITIALIZED in {Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "NO ENV"}");
 
 
-    builder.Services.AddControllers();
     // Agregar los servicios
-    //builder.Services.AddControllers(options =>          // Se agregan los controladores con las configuraciones de los filtros encontrados en FiltersExceptions.cs
-    //{
-    //    options.Filters.Add(typeof(FilterExceptions));
-    //});
+    builder.Services.AddControllers(options =>          // Se agregan los controladores con las configuraciones de los filtros encontrados en FiltersExceptions.cs
+    {
+        options.Filters.Add(typeof(FilterExceptions));
+    });
+
+    // Agregar Swagger para documentacion de la API
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(options =>
+    {
+        options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+        {
+            Title = "Evaluacion Final - Full Stack Dev - Mitocode",
+            Version = "v1",
+            Description = "API para la evaluacion final del curso Full Stack Dev de Mitocode por Andres Evans",
+            Contact = new OpenApiContact
+            {
+                Name = "Andres Evans",
+                Email = "andres_evans@outlook.com",
+                Url = new Uri("https://linkedin.com/in/andresevans")
+            }
+        });
+    });
 
     /**
      * Para arriba estan los servicios!
@@ -39,6 +58,15 @@ try
 
     var app = builder.Build();
 
+    /**
+     * Agregar Swagger 
+     * solo si la aplicacion se esta ejecutando en modo de desarrollo.
+     */
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
 
 
     app.UseHttpsRedirection();
