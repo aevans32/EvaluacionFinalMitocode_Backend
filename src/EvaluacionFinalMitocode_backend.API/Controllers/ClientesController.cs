4e9a1d826b1d8d9f7f781948042c6b438c0f352d
Andres Evans <andres_evans@outlook.com>
2025-08-22 18:10:58 -0500

    Added a controller for Clientes

---
+++ src/EvaluacionFinalMitocode_backend.API/Controllers/ClientesController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EvaluacionFinalMitocode_backend.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClientesController
+    {
+    }
+}
