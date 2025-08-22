b9048d6a7339f0a5b29e4f8621e6267b143f7606
Andres Evans <andres_evans@outlook.com>
2025-08-22 10:34:23 -0500

    Created FilterExceptions for a more appropirate handling of fatal errors

---
+++ src/EvaluacionFinalMitocode_backend.API/Filters/FilterExceptions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace EvaluacionFinalMitocode_backend.API.Filters
+{
+    /**
+     * Cuando sucede una excepcion en la aplicacion a nivel global, se captura y se logea.
+     * Esto es un filtro de excepciones que se aplica a nivel de controlador.
+     */
+    public class FilterExceptions(ILogger<FilterExceptions> logger) : ExceptionFilterAttribute
+    {
+        private readonly ILogger<FilterExceptions> logger = logger;
+        public override void OnException(ExceptionContext context)
+        {
+            logger.LogError(context.Exception, "Un Error ocurrio en la aplicacion.");
+            base.OnException(context);
+        }
+    }
+}
